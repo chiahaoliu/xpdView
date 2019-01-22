f250cc6f2eb3e6d9b47ec94907f596fb8e4346d7
Timothy Liu <chbrahmsliu@gmail.com>
2019-01-22 18:13:40 -0500

    REF: revisit update method of waterfall class
    New data can be added properly through update

--- xpdview/waterfall.py
+++ xpdview/waterfall.py
@@ -64,45 +64,92 @@ class Waterfall:
         # init
         self.update(self.key_list, self.int_data_list, refresh=True)
 
-    def update(self, key_list=None, int_data_list=None, refresh=False):
+    def update(self, key_list, int_data_list, refresh=False):
         """top method to update information carried by class and plot
 
         Parameters
         ----------
-        key_list : list, optional
-            list of keys. default to None.
-        int_data_list : list, optional
-            list of 1D data. default to None.
+        key_list : list
+            list of keys.
+        int_data_list : list
+            list of 1D data.
         refresh : bool, optional
             option to set refresh or not. default to False.
         """
+        # sanity check: stop if no data
         if not int_data_list:
             print("INFO: no reduced data was fed in, "
                   "waterfall plot can't be updated")
             self.halt = True
             return
-        # refresh list
+        # sanity check: both are list
+        if not isinstance(key_list, list) & isinstance(int_data_list, list):
+            raise TypeError("Expect both key_list and int_data_list to "
+                            "be list")
+        # sanity check: both are the same length, if key_list is given
+        if key_list and len(key_list) != len(int_data_list):
+            raise ValueError("Expect key_list and int_data_list "
+                             "to be the same length")
+        # refresh if specified
         if refresh:
             self.key_list = []
             self.int_data_list = []
+        # bookkeeping: assign arrays to class
         self.key_list.extend(key_list)
         self.int_data_list.extend(int_data_list)
-        self._adapt_data_list(self.int_data_list)
-        # generate plot
-        self.halt = False
-        self._update_plot()  # use current value of x,y offset
+        # update dx and dy and x,y array to the class
+        self._adapt_data_list()
+        # generate plot. it will only plot arrays & keys supplied
+        self._update_plot(key_list, int_data_list, refresh)
 
-    def _adapt_data_list(self, int_data_list):
+    def _update_plot(self, key_list, int_data_list, refresh):
+        """core method to update x-, y-offset sliders"""
+        self.halt = False
+        # sanity check: refresh & empty axes
+        if refresh and self.ax.lines:
+            raise ValueError("Expect empty axes when refresh set"
+                             " to True in the update method")
+        # update lines to axes
+        for ind, (x, y) in enumerate(int_data_list):
+            # note: could be better approach
+            if key_list:
+                k = key_list[ind]
+            else:
+                k = '' # this will causes warning when plotting legend
+            self.ax.plot(x, y, picker=5, label=k, **self.kwargs)
+        if self.unit:
+            xlabel, ylabel = self.unit
+            self.ax.set_xlabel(xlabel)
+            self.ax.set_ylabel(ylabel)
+        # draw
+        self._draw_waterfall()
+
+    def _draw_waterfall(self, x_offset_val=None, y_offset_val=None):
+        """core method to draw waterfall plot"""
+        # remain current offset if no new value is passed
+        if not x_offset_val:
+            x_offset_val = self.x_offset_slider.val
+        if not y_offset_val:
+            y_offset_val = self.y_offset_slider.val
+        # update matplotlib line data
+        for i, (l, x, y) in enumerate(zip(self.ax.lines,
+                                          self.x_array_list,
+                                          self.y_array_list)):
+            xx = x+self.xdist*i*x_offset_val
+            yy = y+self.ydist*i*y_offset_val
+            l.set_data(xx, yy)
+        self.ax.relim()
+        self.ax.autoscale_view()
+        self.canvas.draw_idle()
+
+    def _adapt_data_list(self):
         """method to return statefull information of 1D data list"""
         # parse
-        x_dist, y_dist = 0, 0
-        for x, y in int_data_list:
+        for x, y in self.int_data_list:
             self.xdist = max(np.ptp(x), self.xdist)
             self.ydist = max(np.ptp(y), self.ydist)
             self.x_array_list.append(x)
             self.y_array_list.append(y)
-        self.x_dist = x_dist
-        self.y_dist = y_dist
 
     def on_plot_hover(self, event):
         """callback to show legend when click on one of curves"""
@@ -112,43 +159,12 @@ class Waterfall:
                          handletextpad=0, fancybox=True)
         line.figure.canvas.draw_idle()
 
-    def _update_plot(self, x_offset_val=None, y_offset_val=None):
-        """core method to update x-, y-offset sliders"""
-        # remain current offset
-        if not x_offset_val:
-            x_offset_val = self.x_offset_slider.val
-        if not y_offset_val:
-            y_offset_val = self.y_offset_slider.val
-        # draw if fresh axes
-        if not self.ax.get_lines():
-            for ind, el in enumerate(zip(self.x_array_list,
-                                         self.y_array_list,
-                                         self.key_list)):
-                x, y, k = el
-                self.ax.plot(x, y, label=k, picker=5,
-                             **self.kwargs)
-        # update matplotlib line data
-        lines = self.ax.get_lines()
-        for i, (l, x, y) in enumerate(zip(lines,
-                                          self.x_array_list,
-                                          self.y_array_list)):
-            xx = x+self.xdist*i*x_offset_val
-            yy = y+self.ydist*i*y_offset_val
-            l.set_data(xx, yy)
-        self.ax.relim()
-        self.ax.autoscale_view()
-        if self.unit:
-            xlabel, ylabel = self.unit
-            self.ax.set_xlabel(xlabel)
-            self.ax.set_ylabel(ylabel)
-        self.canvas.draw_idle()
-
     def update_y_offset(self, val):
         if self.halt:
             return
-        self._update_plot(None, val)
+        self._draw_waterfall(None, val)
 
     def update_x_offset(self, val):
         if self.halt:
             return
-        self._update_plot(val, None)
+        self._draw_waterfall(val, None)
